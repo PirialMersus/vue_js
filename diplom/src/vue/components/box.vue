9dcdaf5034093132b45a20d94a719f91d9b68f66
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2020-08-03 19:28:29 +0300

    diplom crazy boxes

--- diplom/src/vue/components/box.vue
+++ diplom/src/vue/components/box.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="box">
-    <p>{{ d }}</p>
+  <div
+    class="box"
+    :data-attr="value.id"
+    :style="`transform: translate(${value.x}px, ${value.y}px)`"
+    @transition="pushBoxTransitionData"
+  >
+    <p>
+      {{ distanceX }} {{ value.x }},{{ distanceY }},{{ value.y }}
+      {{ value.id }}
+    </p>
   </div>
 </template>
 <script>
@@ -10,39 +18,102 @@ module.exports = {
       action: false,
       x1: 0,
       x2: 0,
-      distance: 0,
+      y1: 0,
+      y2: 0,
+      distanceX: 0,
+      distanceY: 0,
+      boxCopys: [],
+      savedPositions: [],
+      savePositionX: 0,
+      savePositionY: 0,
+      currentBoxData: 0,
     };
   },
-  computed: {
-    d: function () {
-      return (this.distance = this.x2 - this.x1);
+  props: ['value', 'boxesCopy'],
+  methods: {
+    boxController: function (positionX, positionY) {
+      $currentBox.css(
+        'transform',
+        'translate(' + positionX + 'px, ' + positionY + 'px)',
+      );
+    },
+    pushBoxTransitionData: function () {
+      this.$emit(
+        'transition',
+        this.currentBoxData,
+        this.distanceX,
+        this.distanceY,
+      );
+      // document.querySelector('.box').style.transform =
+      //   'translate(' + this.distanceX + 'px,' + this.distanceY + 'px)';
+      // document.querySelector('.box').style.transform = 'rotate(45deg)';
+      // console.log('boxController: я работаю');
     },
   },
-  props: [],
-  methods: {},
+  created: function () {},
+  // updated: function () {
+  //   this.boxCopys = this.boxesCopy;
+  // },
   mounted: function () {
-    document.querySelector('.box').addEventListener('mousedown', function (e) {
-      console.log('start');
-      console.log(e.pageX);
-      this.x1 = e.pageX;
-      this.action = true;
-    });
-    window.addEventListener('mouseup', function (e) {
-      console.log('end');
-      this.x2 = e.pageX;
-      console.log(e.pageX);
-      this.action = false;
-    });
-    window.addEventListener('mousemove', function (e) {
-      console.log(this.x1);
-      if (this.action) {
-        console.log('move');
-        console.log(e.pageX);
-        this.x2 = e.pageX;
-        this.distance = this.x2 - this.x1;
-      }
-      console.log(this.distance);
-    });
+    $(document).on(
+      'mousedown',
+      '.box',
+      function (e) {
+        let $currentBox = $(this.$el);
+        this.currentBoxData = $currentBox.attr('data-attr');
+        // this.currentBoxData = 0;
+        this.x1 = e.pageX;
+        this.y1 = e.pageY;
+        this.action = true;
+        // console.log(this.boxCopys[0], this.boxCopys[1], this.boxCopys[2]);
+        // console.log(this.currentBoxData);
+      }.bind(this),
+    );
+
+    // $(document).on('mousedown', '.box', function (e) {
+    //   $currentBox = $(this);
+    //   currentBoxData = $currentBox.attr('data-attr');
+    //   console.log(currentBoxData);
+    //   x = e.pageX;
+    //   y = e.pageY;
+    //   this.action = true;
+    // });
+
+    window.addEventListener(
+      'mouseup',
+      function (e) {
+        this.action = false;
+        this.pushBoxTransitionData();
+        // this.savedPositionX = this.distanceX;
+        // this.savedPositionY = this.distanceY;
+        this.savedPositions.push({ x: this.distanceX, y: this.distanceY });
+      }.bind(this),
+    );
+
+    window.addEventListener(
+      'mousemove',
+      function (e) {
+        if (this.action) {
+          // console.log('move');
+          // this.x2 = e.pageX;
+          // this.y2 = e.pageY;
+          if (this.savedPositions[this.currentBoxData].x) {
+            this.distanceX =
+              this.savedPositions[this.currentBoxData].x + (e.pageX - this.x1);
+            this.distanceY =
+              this.savedPositions[this.currentBoxData].y + (e.pageY - this.y1);
+          } else {
+            this.distanceX = e.pageX - this.x1;
+            this.distanceY = e.pageY - this.y1;
+          }
+          boxController(this.distanceX, this.distanceY);
+          // this.distanceX = this.savedPositionX + (this.x2 - this.x1);
+          // this.distanceY = this.savedPositionY + (this.y2 - this.y1);
+        }
+        // console.log('this.distanceX', this.distanceX);
+        // console.log('this.distanceY', this.distanceY);
+      }.bind(this),
+    );
   },
 };
 </script>
--- diplom/src/vue/index.vue
+++ diplom/src/vue/index.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="global-container">
+    <button @click="addNewBox">Add new box</button>
     <div class="containerForBoxElement">
-      <box></box>
+      <box
+        :boxesCopy="boxes"
+        v-for="item in boxes"
+        :value="item"
+        @transition="setTransition"
+      ></box>
     </div>
   </div>
 </template>
 <script>
 module.exports = {
   data: function () {
-    return {};
+    return {
+      counter: 0,
+      boxes: [],
+    };
   },
   components: {
     box: require('./components/box.vue'),
   },
-  methods: {},
+  methods: {
+    addNewBox: function () {
+      this.boxes.push({ id: this.counter, x: 0, y: 0 });
+      this.counter++;
+    },
+    setTransition: function (index, transitionX, transitionY) {
+      // console.log(index, transitionX, transitionY);
+      let tempValueX = this.boxes[index].x;
+      let tempValueY = this.boxes[index].y;
+      this.boxes[index].x = tempValueX + transitionX;
+      this.boxes[index].y = tempValueY + transitionY;
+    },
+  },
 };
 </script>
