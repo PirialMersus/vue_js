bf56054310b4dacaa8721d7bc7a266ef13cc74a5
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2020-08-04 17:13:50 +0300

    questions to andrew

--- diplom/src/vue/components/box.vue
+++ diplom/src/vue/components/box.vue
@@ -22,21 +22,24 @@ module.exports = {
       y2: 0,
       distanceX: 0,
       distanceY: 0,
-      boxCopys: [],
       savedPositions: [],
       savePositionX: 0,
       savePositionY: 0,
       currentBoxData: 0,
+      $currentBox: 0,
     };
   },
-  props: ['value', 'boxesCopy'],
+
+  props: ['value'],
+
   methods: {
     boxController: function (positionX, positionY) {
-      $currentBox.css(
+      this.$currentBox.css(
         'transform',
         'translate(' + positionX + 'px, ' + positionY + 'px)',
       );
     },
+
     pushBoxTransitionData: function () {
       this.$emit(
         'transition',
@@ -44,49 +47,37 @@ module.exports = {
         this.distanceX,
         this.distanceY,
       );
-      // document.querySelector('.box').style.transform =
-      //   'translate(' + this.distanceX + 'px,' + this.distanceY + 'px)';
-      // document.querySelector('.box').style.transform = 'rotate(45deg)';
-      // console.log('boxController: я работаю');
     },
   },
+
   created: function () {},
-  // updated: function () {
-  //   this.boxCopys = this.boxesCopy;
-  // },
+
   mounted: function () {
     $(document).on(
       'mousedown',
       '.box',
       function (e) {
-        let $currentBox = $(this.$el);
-        this.currentBoxData = $currentBox.attr('data-attr');
-        // this.currentBoxData = 0;
+        this.$currentBox = $(this.$el);
+        this.currentBoxData = this.$currentBox.attr('data-attr');
+
+        if (typeof this.savedPositions[this.currentBoxData] == undefined) {
+          savedPositions[this.currentBoxData] = { x: 0, y: 0 };
+        }
         this.x1 = e.pageX;
         this.y1 = e.pageY;
         this.action = true;
-        // console.log(this.boxCopys[0], this.boxCopys[1], this.boxCopys[2]);
-        // console.log(this.currentBoxData);
       }.bind(this),
     );
-
-    // $(document).on('mousedown', '.box', function (e) {
-    //   $currentBox = $(this);
-    //   currentBoxData = $currentBox.attr('data-attr');
-    //   console.log(currentBoxData);
-    //   x = e.pageX;
-    //   y = e.pageY;
-    //   this.action = true;
-    // });
 
     window.addEventListener(
       'mouseup',
       function (e) {
         this.action = false;
         this.pushBoxTransitionData();
-        // this.savedPositionX = this.distanceX;
-        // this.savedPositionY = this.distanceY;
-        this.savedPositions.push({ x: this.distanceX, y: this.distanceY });
+        this.savedPositions[this.currentBoxData] = {
+          x: this.distanceX,
+          y: this.distanceY,
+        };
       }.bind(this),
     );
 
@@ -94,24 +85,12 @@ module.exports = {
       'mousemove',
       function (e) {
         if (this.action) {
-          // console.log('move');
-          // this.x2 = e.pageX;
-          // this.y2 = e.pageY;
-          if (this.savedPositions[this.currentBoxData].x) {
-            this.distanceX =
-              this.savedPositions[this.currentBoxData].x + (e.pageX - this.x1);
-            this.distanceY =
-              this.savedPositions[this.currentBoxData].y + (e.pageY - this.y1);
-          } else {
-            this.distanceX = e.pageX - this.x1;
-            this.distanceY = e.pageY - this.y1;
-          }
-          boxController(this.distanceX, this.distanceY);
-          // this.distanceX = this.savedPositionX + (this.x2 - this.x1);
-          // this.distanceY = this.savedPositionY + (this.y2 - this.y1);
+          this.distanceX =
+            this.savedPositions[this.currentBoxData].x + (e.pageX - this.x1);
+          this.distanceY =
+            this.savedPositions[this.currentBoxData].y + (e.pageY - this.y1);
+          this.boxController(this.distanceX, this.distanceY);
         }
-        // console.log('this.distanceX', this.distanceX);
-        // console.log('this.distanceY', this.distanceY);
       }.bind(this),
     );
   },
